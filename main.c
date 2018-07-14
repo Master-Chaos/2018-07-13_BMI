d69a82f3116b480fecd3623954e6f1d26cedd3e6
Master-Chaos <thomas_pointner@gmx.at>
2018-07-14 10:50:51 +0200

    some refactor of vars and make comments

--- main.c
+++ main.c
@@ -1,84 +1,81 @@
 #include <stdio.h>
 #include <string.h>
 
-float calulateBMI(float *size, float *weigth)
+float calculateBMI(float *size, float *weight) //CALCULATE BMI
 {
-    float bmi = 0;
+    float bmi = 0; //INIT VARIABLE BMI
 
-    bmi = (*weigth / (*size * *size));
+    bmi = (*weight / (*size * *size)); //FORMULA TO CALCULATE BMI
 
-    printf("Your BMI is: %.2f\n", bmi);
+    printf("Your BMI is: %.1f\n", bmi); //PRINT OUT RESULT OF BMI
 
 return bmi;
 }
 
-void checkoptimalBMI (int age)
+void checkoptimalBMI (int age) //CHECK OPTIMAL BMI BY AGE
 {
-    printf("Your optimal BMI is: ");
-    switch(age)
+    printf("Your optimal BMI is: "); //PRINT OUT FIRST PART
+    switch(age) //TAKE CASE BY AGE AND DO THE INSTRUCTION OF THE USED CASE
     {
-        case 0 ... 18: printf("\n");
-        case 19 ... 24: printf("19-24\n");
-        case 25 ... 34: printf("20-25\n");
-        case 35 ... 44: printf("21-26\n");
-        case 45 ... 54: printf("22-27\n");
-        case 55 ... 64: printf("23-28\n");
-        default: printf("24-29\n");
+        case 0 ... 18: printf("-\n");break;
+        case 19 ... 24: printf("19-24\n");break;
+        case 25 ... 34: printf("20-25\n");break;
+        case 35 ... 44: printf("21-26\n");break;
+        case 45 ... 54: printf("22-27\n");break;
+        case 55 ... 64: printf("23-28\n");break;
+        default: printf("24-29\n");break;;
     }
 }
 
-void checkBMI(float bmi, char sex[])
+void checkBMI(float bmi, char sex[]) //CHECK BMI BY CALCULATED BMI AND BY SEX
 {
-    int bmivalue = (int) (bmi+0.5);
+    int bmitoint = (int) (bmi+0.5); //CONVERT BMI FROM FLOAT TO INT
 
-    if(strcmp(sex, "male") == 0)
+    if(strcmp(sex, "male") == 0) //COMPARE SEX WITH MALE; IF CONDITION IS TRUE DO THIS
     {
-        switch(bmivalue)
+        switch(bmitoint) //TAKE CASE BY BMI AND DO THE INSTRUCTION OF THE USED CASE
         {
-            case 0 ... 19: printf("You are underweight\n"); break;
-            case 20 ... 25: printf("You have normal weight\n"); break;
-            case 26 ... 30: printf("You have overweight\n"); break;
-            case 31 ... 40: printf("You have adiposity\n"); break;
-            default: printf("You have massive adiposity\n"); break;
+            case 0 ... 19: printf("You are underweight!\n"); break;
+            case 20 ... 25: printf("You have normal weight.\n"); break;
+            case 26 ... 30: printf("You have overweight!\n"); break;
+            case 31 ... 40: printf("You have adiposity!!\n"); break;
+            default: printf("You have massive adiposity!!!\n"); break;
         }
 
     }
-    else if (strcmp(sex,"female") == 0)
+    else if (strcmp(sex,"female") == 0) //COMPARE SEX WITH FEMALE; IF CONDITION IS TRUE DO THIS
     {
-        switch(bmivalue)
+        switch(bmitoint) //TAKE CASE BY BMI AND DO THE INSTRUCTION OF THE USED CASE
         {
-            case 0 ... 18: printf("You are underweight\n"); break;
-            case 19 ... 24: printf("You have normal weight\n"); break;
-            case 25 ... 30: printf("You have overweight\n"); break;
-            case 31 ... 40: printf("You have adiposity\n"); break;
-            default: printf("You have massive adiposity\n"); break;
+            case 0 ... 18: printf("You are underweight!\n"); break;
+            case 19 ... 24: printf("You have normal weight.\n"); break;
+            case 25 ... 30: printf("You have overweight!\n"); break;
+            case 31 ... 40: printf("You have adiposity!!\n"); break;
+            default: printf("You have massive adiposity!!!\n"); break;
         }
     }
-    else
+    else //IF SEX IS NOT MALE OR FEMALE; THAN PRINT OUT THE FOLLOWING INFO
     {
         printf("Your could not be deceived! Please try again.\n");
     }
 }
 
 
-int main() {
+int main() { //MAIL FUNCTION
 
-    float size = 1.84;
-    float weigth = 94;
-    float bmi;
-    int age = 26;
-    char sex[10] = "male";
+    float size = 1.5; //INPUT YOUR SIZE IN M
+    float weight = 50; //INPUT YOUR WEIGHT IN KG
+    float bmi; //VARIABLE FOR BMI
+    int age = 20; //INPUT YOUR AGE
+    char sex[7] = "female"; //INPUT YOUR SEX AS MALE OR FEMALE
 
-
-    printf("BMI - Body-Mass-Index-Calculator!\n");
-
-    bmi = calulateBMI(&size, &weigth);
-    checkoptimalBMI(age);
-
-    checkBMI(bmi, sex);
-
-
-
+    printf("------------------------------------------\n"); //OUTPUT SEPARATION
+    printf("BMI - Body-Mass-Index-Calculator!\n"); //PRINT OUT PROGRAM TITLE
+    printf("------------------------------------------\n"); //OUTPUT SEPARATION
+    bmi = calculateBMI(&size, &weight); // PRINT OUT YOUR BMI
+    checkoptimalBMI(age); // PRINT OUT YOUR OPTIMAL BMI
+    checkBMI(bmi, sex); // PRINT OUT YOU BMI-STATUS
+    printf("------------------------------------------\n"); //OUTPUT SEPARATION
 
     return 0;
 }
